92135ad221843bcb708895b79df190bf365aae1b
sp <sp@gmail.com>
2025-07-22 15:25:45 +0900

    Add getGop feature

--- dtream1/main.cpp
+++ dtream1/main.cpp
@@ -7,12 +7,15 @@ public:
 	int getSumSum(int a, int b, int c) {
 		return a + b + c;
 	}
+
+	int getGop(int a, int b) {
+		return a * b;
+	}
 };
 
 TEST(tc, tc1) {
 	EXPECT_EQ(1, 1);
 }
-
 
 int main() {
 	::testing::InitGoogleMock();
