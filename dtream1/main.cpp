6d81bd2d59d446267a93ea1d8e5abdb7da614c24
yenna <yena.gwak@gmail.com>
2025-07-22 15:22:40 +0900

    add main

--- dtream1/main.cpp
+++ dtream1/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <gmock/gmock.h>
+
+class Cal {
+public:
+
+	int getSumSum(int a, int b, int c) {
+		return a + b + c;
+	}
+};
+
+TEST(tc, tc1) {
+	EXPECT_EQ(1, 1);
+}
 
 
 int main() {
-
-
-
+	::testing::InitGoogleMock();
+	return RUN_ALL_TESTS();
 }
