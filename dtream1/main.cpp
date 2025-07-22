b6b494f4032412e7f12ca60c8a576bbd69b7505c
Donghyuk.Yang <sency90@gmail.com>
2025-07-22 15:23:42 +0900

    add unit test for getSum()

--- dtream1/main.cpp
+++ dtream1/main.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
+#include "gmock/gmock.h"
+using namespace testing;
 
-int getSum(int a, int b) {
-	return a+b;
+class Cal {
+public:
+	int getSum(int a, int b) {
+		return a+b;
+	}
+};
+
+class CalculatorFixture: public Test {
+public:
+	Cal cal;
+	void test(int expected, int actual) {
+		EXPECT_EQ(expected, actual);
+	}
+};
+TEST_F(CalculatorFixture, TC1) {
+	test(getSum(1, 2), 3);
+}
+
+TEST_F(CalculatorFixture, TC2) {
+	test(getSum(1e8, 3e8), 4e8);
+}
+
+TEST_F(CalculatorFixture, TC3) {
+	test(getSum(1U<<31, 1U<<31))
 }
 
 
+
+
 int main() {
-	return 0;
+	::testing::InitGoogleMock();
+	return RUN_ALL_TESTS();
+
 }
