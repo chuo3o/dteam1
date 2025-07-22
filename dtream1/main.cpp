31209a267740bf9308a17b703feb2dc9c9946fdc
sp <sp@gmail.com>
2025-07-22 15:37:43 +0900

    Add GetGop TEST Case

--- dtream1/main.cpp
+++ dtream1/main.cpp
@@ -10,6 +10,7 @@ public:
 
 	int getGop(int a, int b) {
 		return a * b;
+	}
 
 	int getZegop(int a) {
 		return a * a;
@@ -18,6 +19,14 @@ public:
 
 TEST(tc, tc1) {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(tc, gop) {
+    Cal cal;
+    EXPECT_EQ(2, cal.getGop(1, 2));
+    EXPECT_EQ(6, cal.getGop(2, 3));
+    EXPECT_EQ(15, cal.getGop(3, 5));
+    EXPECT_EQ(36, cal.getGop(4, 9));
 }
 
 TEST(tc, zegop) {
